2abd9bcaf994a481052e20ed20068b22087791b9
ue-git <67855463+ue-git@users.noreply.github.com>
2026-02-27 02:13:53 +0000

    Add 2 more sports related activities, 2 more artistic activities, and 2 more intellectual activities.

--- src/app.py
+++ src/app.py
@@ -62,6 +62,9 @@ def signup_for_activity(activity_name: str, email: str):
     # Get the specific activity
     activity = activities[activity_name]
 
+    # Validate student is not already signed up
+    
+
     # Add student
     activity["participants"].append(email)
     return {"message": f"Signed up {email} for {activity_name}"}
